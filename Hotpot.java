a8af56d5042cb8073d3d13694572115e287f74b1
Kimberly <156276109+K1mcheee@users.noreply.github.com>
2026-04-07 13:51:55 +0800

    Add template files

---
+++ Hotpot.java
@@ -0,0 +1,40 @@
+import java.util.List;
+import java.util.stream.Stream;
+
+public class Hotpot {
+    private List<Ingredient> ingredients;
+
+    public Hotpot(List<Ingredient> ingredients) {
+        this.ingredients = ingredients;
+    }
+
+    public List<Ingredient> cook() {
+        return null;
+    }
+
+    public String chop(long n) {
+        return "";
+    }
+
+    public long mostOrderedAmount() {
+        return 0L;
+    }
+
+    public List<Ingredient> takeUnique() {
+        return null; // if count is 0, its first occurrence so keep it
+    }
+
+
+    public String combine(Hotpot other) { 
+        return "";
+    }
+
+    public static List<Pair<Ingredient, Double>> calculate(List<Pair<Pair<Ingredient,String>, Double>> bowl, List<Pair<String, Double>> price) {
+        return null;
+        
+    }
+
+    public String mostOrdered() {
+        return "";
+    }
+}
